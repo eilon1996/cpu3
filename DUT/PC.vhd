8f3dbf8cbfac712fb3f3f34fc368306e1fe22999
בארי יוסילביץ <barryyos@auth.ad.bgu.ac.il>
2022-05-15 17:54:01 +0300

    updated Datapath unit

--- DUT/PC.vhd
+++ DUT/PC.vhd
@@ -9,7 +9,7 @@ entity PC is
     port(	clk, PCin: in std_logic;
             PCsel:	in std_logic_vector(1 downto 0);
             offset:	in std_logic_vector(4 downto 0);
-            PC_value :buffer std_logic_vector(Dwidth-1 downto 0);
+            PC_out: out std_logic_vector(Dwidth-1 downto 0)
         );
 end PC;
 ------------- complete the ALU Architecture code --------------
@@ -22,7 +22,7 @@ begin
 plus1_pm: AdderSub port map(
     sub => '0',
     carry_in => '1',
-    a => PC_value,
+    a => PC_out,
     b => (others => '0'),
     result => PC_value_plus1,
 );
@@ -36,7 +36,7 @@ plus_offset_pm: AdderSub port map(
 );
 
 WITH PCsel SELECT
-PC_value    <=  (others => 0)        when "00",
+PC_out    <=  (others => 0)        when "00",
                 PC_value_plus_offset when "01",
                 PC_value_plus1       when "10",
                 UNAFFECTED when others ;
